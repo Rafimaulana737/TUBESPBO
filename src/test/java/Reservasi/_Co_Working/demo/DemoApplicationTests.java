5403a17debb82bb20f84bb95ffe71863ebd9c109
Rafimaulana737 <kanakloteng10@gmail.com>
2026-06-23 13:03:22 +0700

    pertama

---
+++ src/test/java/Reservasi/_Co_Working/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Reservasi._Co_Working.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
