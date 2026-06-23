a9cc745a0fb2448f0a6b0c7e4e1975e5c2080309
Rafimaulana737 <kanakloteng10@gmail.com>
2026-06-23 20:39:55 +0700

    nambah dikit

--- src/main/java/Reservasi/_Co_Working/demo/controller/UserController.java
+++ src/main/java/Reservasi/_Co_Working/demo/controller/UserController.java
@@ -1,45 +1,55 @@
 package Reservasi._Co_Working.demo.controller;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import Reservasi._Co_Working.demo.model.User;
 import Reservasi._Co_Working.demo.service.UserService;
 
-@RestController
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 @RequestMapping("/user")
 public class UserController {
+
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping
+    
+
+    @GetMapping("/page")
+    public String usersPage(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "users";
+    }
+
+    
+
+    @GetMapping("/api")
+    @ResponseBody
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/api/{id}")
+    @ResponseBody
     public User getUserById(@PathVariable Long id) {
         return userService.getUserById(id).orElse(null);
     }
 
-    @PostMapping
+    @PostMapping("/api")
+    @ResponseBody
     public User createUser(@RequestBody User user) {
-    return userService.saveUser(user);
+        return userService.saveUser(user);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/{id}")
+    @ResponseBody
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
     }
-
 }
